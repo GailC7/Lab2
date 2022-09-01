2a686a0262c54551d0a5f89cdfbb5d739a5f9930
GailC7 <chintalekhag@etsu.edu>
2022-09-01 17:13:54 -0400

    Step 1 done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class lab2
+{
+    static void Main(string[] args)
+    {
+        // #1
+        Console.WriteLine("Enter a number");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter a second number");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(num1 + " + " + num2 + " = " + (num1+num2));
+
+        // #2
+        Console.WriteLine("Enter a number");
+        int multiNum = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("How high to multiply by");
+        int user = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(multiNum * user);
+    }
+
+}
+
