024202600546c29ad82899e910a25b3a03bc4719
GailC7 <chintalekhag@etsu.edu>
2022-09-01 18:27:13 -0400

    Download console table

--- Program.cs
+++ Program.cs
@@ -28,11 +28,58 @@ public class lab2
         }
 
 
+
         // #3
         Console.WriteLine($"{sizeof(decimal)}");
 
         ConsoleTable table = new ConsoleTable("Name");
    
+
+        // #4
+        
+
+        bool exit
+        if(exit =! true)
+        { 
+            Console.WriteLine("Enter a number");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Select an operation + , - , * , or / ");
+            String op = Console.ReadLine();
+
+            Console.WriteLine("Enter a second number");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            switch(op)
+            {
+                case '+': 
+                    Console.WriteLine(number1 + " + " + number2 + " = " + (number1 + number2);
+                    break;
+
+                case '-': 
+                    Console.WriteLine(number1 + " - " + number2 + " = " + (number1 - number2);
+                    break;
+
+                 case '*': 
+                    Console.WriteLine(number1 + " * " + number2 + " = " + (number1 * number2);
+                    break;
+
+                 case '/': 
+                    Console.WriteLine(number1 + " / " + number2 + " = " + (number1 / number2);
+                    break;
+
+                  default:
+                    Console.WriteLine("Operation Error");
+            }
+
+            
+        }
+        else
+        { 
+            
+        }
+
+       
     }
 
 }
