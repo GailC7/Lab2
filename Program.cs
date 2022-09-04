1fcb85dda8c402ecbf7ce66368b7aa6ba803e33a
GailC7 <chintalekhag@etsu.edu>
2022-09-04 00:34:52 -0400

    Table complete

--- Program.cs
+++ Program.cs
@@ -33,24 +33,24 @@ public class lab2
         //Console.WriteLine($"{sizeof(decimal)}");
 
         ConsoleTable table = new ConsoleTable("Type", "Byte(s) of Memory", "Min", "Max");
-        table.AddRow("sbyte", 1, -128, 127);
-        table.AddRow("byte", 1, 0, 255);
-        table.AddRow("short", 2, -32768, 32767);
-        table.AddRow("ushort", 2, 0, 65535);
-        table.AddRow("int", 4, -2147483648, 2147483647);
-        table.AddRow("uint", 4, 0, 4294967295);
-        table.AddRow("long", 8, -9223372036854775808, 9223372036854775807);
-        table.AddRow("ulong", 8, 0, 18446744073709551615);
-        table.AddRow("float", 4, "-3.4028235E+38", "3.4025235E+38");
-        table.AddRow("double", 8, "-1.7976931348623157E+308", "1.7976931348623157E+308");
-        table.AddRow("decimal", 16, "-79228162514264337593543950335", "79228162514264337593543950335");
+        table.AddRow("sbyte", "1" , "-128" , "127");
+        table.AddRow("byte", "1", "0", "255");
+        table.AddRow("short", "2", "-32768", "32767");
+        table.AddRow("ushort", "2", "0", "65535");
+        table.AddRow("int", "4", "-2147483648", "2147483647");
+        table.AddRow("uint", "4", "0", "4294967295");
+        table.AddRow("long", "8", "-9223372036854775808", "9223372036854775807");
+        table.AddRow("ulong", "8", "0", "18446744073709551615");
+        table.AddRow("float", "4", "-3.4028235E+38", "3.4025235E+38");
+        table.AddRow("double", "8", "-1.7976931348623157E+308", "1.7976931348623157E+308");
+        table.AddRow("decimal", "16", "-79228162514264337593543950335", "79228162514264337593543950335");
+
+        table.Write();
 
         // #4
-
-
         bool exit;
-        if(exit =! true)
-        { 
+        if (exit = !true)
+        {
             Console.WriteLine("Enter a number");
             int number1 = Convert.ToInt32(Console.ReadLine());
 
@@ -60,36 +60,39 @@ public class lab2
             Console.WriteLine("Enter a second number");
             int number2 = Convert.ToInt32(Console.ReadLine());
 
-            switch(op)
+            switch (op)
             {
-                case '+': 
+                case '+':
                     Console.WriteLine(number1 + " + " + number2 + " = " + (number1 + number2);
                     break;
 
-                case '-': 
+                case '-':
                     Console.WriteLine(number1 + " - " + number2 + " = " + (number1 - number2);
                     break;
 
-                 case '*': 
+                case '*':
                     Console.WriteLine(number1 + " * " + number2 + " = " + (number1 * number2);
                     break;
 
-                 case '/': 
+                case '/':
                     Console.WriteLine(number1 + " / " + number2 + " = " + (number1 / number2);
                     break;
 
-                  default:
+                default:
                     Console.WriteLine("Operation Error");
             }
 
-            
+
         }
         else
-        { 
-            
+        {
+
         }
 
-       
+
+
+
+
     }
 
 }
