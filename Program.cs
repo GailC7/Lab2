2d1415ccdb0a06d730a970f80e2a4773e8ebfe35
GailC7 <chintalekhag@etsu.edu>
2022-09-04 00:28:49 -0400

    table fill

--- Program.cs
+++ Program.cs
@@ -30,15 +30,25 @@ public class lab2
 
 
         // #3
-        Console.WriteLine($"{sizeof(decimal)}");
+        //Console.WriteLine($"{sizeof(decimal)}");
 
-        ConsoleTable table = new ConsoleTable("Name");
-   
+        ConsoleTable table = new ConsoleTable("Type", "Byte(s) of Memory", "Min", "Max");
+        table.AddRow("sbyte", 1, -128, 127);
+        table.AddRow("byte", 1, 0, 255);
+        table.AddRow("short", 2, -32768, 32767);
+        table.AddRow("ushort", 2, 0, 65535);
+        table.AddRow("int", 4, -2147483648, 2147483647);
+        table.AddRow("uint", 4, 0, 4294967295);
+        table.AddRow("long", 8, -9223372036854775808, 9223372036854775807);
+        table.AddRow("ulong", 8, 0, 18446744073709551615);
+        table.AddRow("float", 4, "-3.4028235E+38", "3.4025235E+38");
+        table.AddRow("double", 8, "-1.7976931348623157E+308", "1.7976931348623157E+308");
+        table.AddRow("decimal", 16, "-79228162514264337593543950335", "79228162514264337593543950335");
 
         // #4
-        
 
-        bool exit
+
+        bool exit;
         if(exit =! true)
         { 
             Console.WriteLine("Enter a number");
